f84481a812c2e8cbacb75ddb9afee26af7780e6b
alifhredoy <mdalifhasanredoy@gmail.com>
2025-12-21 20:02:41 +0600

    core : react dom setup

---
+++ src/components/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Contact = () => {
+  return (
+    <div>
+        <Link to="/"><h1>Go to Home</h1></Link>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In maiores recusandae perferendis laudantium, harum quo, dolorem, aut eum animi cupiditate velit delectus cumque ea fuga et! Totam quidem temporibus harum eius aspernatur, qui, nostrum atque explicabo pariatur quia mollitia minus delectus deleniti veniam dolore velit quod ducimus dolor inventore placeat recusandae itaque, cupiditate vero. Natus eaque explicabo assumenda. Officiis mollitia, illo debitis ipsum vitae quisquam aliquid minus nisi voluptas aut distinctio numquam commodi, libero non molestias illum. Harum iste eaque, repellendus corrupti amet maiores est quae non quas magni perspiciatis error facere labore, sint commodi dolor obcaecati neque, dignissimos adipisci?</p></div>
+  )
+}
+
+export default Contact
