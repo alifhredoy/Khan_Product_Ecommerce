cbda20a52f4402cb31c68ad814ff9a37a1e3be2b
alifhredoy <mdalifhasanredoy@gmail.com>
2025-12-24 16:23:59 +0600

    core: setup

---
+++ src/layouts/NewsPart.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewsPart = () => {
+  return (
+    <section>
+        
+    </section>
+  )
+}
+
+export default NewsPart
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NewsPart from '../layouts/NewsPart'
+
+
+
+const Home = () => {
+  return (
+    <div>
+     <NewsPart/>
+    </div>
+  )
+}
+
+export default Home
