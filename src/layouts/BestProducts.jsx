0ba039dce539e30e4cd2cdf5b1f47be47fa64ca1
alifhredoy <mdalifhasanredoy@gmail.com>
2026-02-24 13:44:01 +0600

    feat: add API

--- src/layouts/BestProducts.jsx
+++ src/layouts/BestProducts.jsx
@@ -1,21 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Container from '../components/Container'
 import SubHeading from '../components/SubHeading'
 import Heading from '../components/Heading'
 import CardBest from '../components/CardBest'
 import Button from '../components/Button'
-import bestone from '../assets/bestone.png'
-import besttwo from '../assets/besttwo.png'
-import bestthree from '../assets/bestthree.png'
-import bestfour from '../assets/bestfour.png'
-
 import Flex from '../components/Flex'
 
 import "slick-carousel/slick/slick.css";
 
+const BestProducts = () => {
 
+    let [show, setShow] = useState(4)
+    let [Apidata, setApiData] = useState([])
+    let [viewAllClicked, setViewAllClicked] = useState(false)
 
-const TodayProducts = () => {
+    useEffect(() => {
+        fetch("https://dummyjson.com/products")
+            .then((res) => res.json())
+            .then((data) => setApiData(data.products))
+    }, [])
+
+    const handleViewAll = () => {
+        setShow(show + 4)
+        setViewAllClicked(true)
+    }
+
+    const handleLoadMore = () => {
+        setShow(show + 4)
+    }
+
     return (
         <section className='pb-[140px]'>
             <Container>
@@ -26,30 +39,54 @@ const TodayProducts = () => {
                         <Heading className='pt-6 pb-[60px]' text="Best Selling Products" />
                     </div>
 
-                    <Button text="View All" />
+                    {
+                        !viewAllClicked && (
+                            <div onClick={handleViewAll}>
+                                <Button text="View All" />
+                            </div>
+                        )
+                    }
 
                 </Flex>
-                <Flex className='justify-between'>
-                    <div>
-                        <CardBest image={besttwo} title='The north coat' saleprice='260' regularprice='360' />
-                    </div>
-                    <div>
-                        <CardBest image={bestthree} title='Gucci duffle bag' saleprice='960' regularprice='1160' />
-                    </div>
-                    <div>
-                        <CardBest image={bestfour} title='RGB liquid CPU Cooler ' saleprice='160' regularprice='170' />
-                    </div>
-                    <div>
-                        <CardBest image={bestone} title='Small BookSelf' saleprice='360' regularprice='460' />
-                    </div>
 
-
-
+                <Flex className='justify-between flex-wrap gap-y-10'>
+                    <div className="flex flex-wrap gap-4">
+                        {
+                            Apidata.slice(0, show).map(item => (
+                                <div key={item.id}>
+                                    <CardBest
+                                        image={item.thumbnail}
+                                        title={item.title}
+                                        saleprice="280"
+                                        regularprice="320"
+                                    />
+                                </div>
+                            ))
+                        }
+                    </div>
                 </Flex>
+
+                {
+                    viewAllClicked && (
+                        <div className="text-center mt-20">
+                            {
+                                show < Apidata.length ? (
+                                    <div onClick={handleLoadMore}>
+                                        <Button text="Load More Products" />
+                                    </div>
+                                ) : (
+                                    <p className="text-3xl text-red-500">
+                                        No More Products
+                                    </p>
+                                )
+                            }
+                        </div>
+                    )
+                }
 
             </Container>
         </section>
     )
 }
 
-export default TodayProducts
+export default BestProducts
