eb7fa35833071e9f0bff4ab3069a252e00b96d0e
alifhredoy <mdalifhasanredoy@gmail.com>
2026-03-08 12:34:08 +0600

    feat: add cart page & breadcrumb

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
 import Banner from '../layouts/Banner'
 import TodayProducts from '../layouts/TodayProducts'
 import Catergory from '../layouts/Catergory'
@@ -13,25 +12,34 @@ import Support from '../layouts/Support'
 
 
 
-
-
-
+import { useDispatch } from "react-redux"
+import { addbreadcrumb } from "../slices/breadCrumbSlice"
 
 const Home = () => {
+
+const dispatch = useDispatch()
+useEffect(()=>{
+  dispatch(addbreadcrumb("Home"))
+},[])
+
+
+
+
+
+
+
+
+
   return (
     <>
-    <Banner/>
-    <TodayProducts/>
-    <Catergory/>
-    <BestProducts/>
-    <Promotion/>
-    <ExploreProducts/>
-    <NewArrival/>
-    <Support/>
-    
-    
-    
-    
+      <Banner/>
+      <TodayProducts/>
+      <Catergory/>
+      <BestProducts/>
+      <Promotion/>
+      <ExploreProducts/>
+      <NewArrival/>
+      <Support/>
     </>
   )
 }
